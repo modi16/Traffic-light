566e99215a4c440e06ab9f0b5efde0eb8056cf7e
modi16 <dimm8187@gmail.com>
2019-06-26 02:11:23 +0000

    3rd commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,24 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export class Traffic extends React.Component {
+	constructor() {
+		super();
+		this.state = { selected: false };
+	}
+
+	onchange = e => {
+		this.setState = { selected: !this.state.selected };
+	};
+
 	render() {
 		return (
 			<div>
 				<div className="sup text-center" />
 				<div className="container">
-					<div className="red-light" />
-					<div className="yellow-light" />
-					<div className="green-light" />
+					<div className="red-light" onClick={this.onchange} />
+					<div className="yellow-light" onClick={this.onchange} />
+					<div className="green-light" onClick={this.onchange} />
 				</div>
 			</div>
 		);
