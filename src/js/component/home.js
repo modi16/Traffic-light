ce73272fd108f24b684f8daa12f5043f66d6f436
modi16 <dimm8187@gmail.com>
2019-06-29 02:21:26 +0000

    final commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,21 +4,42 @@ import React from "react";
 export class Traffic extends React.Component {
 	constructor() {
 		super();
-		this.state = { selected: false };
+		this.state = { clicked: null };
 	}
 
-	onchange = e => {
-		this.setState = { selected: !this.state.selected };
-	};
-
 	render() {
+		console.log(this.state);
+		let addRedClass = "";
+		if (this.state.clicked == "red") {
+			addRedClass = "selected";
+		}
+
+		let addGreenClass = "";
+		if (this.state.clicked == "green") {
+			addGreenClass = "selected";
+		}
+
+		let addYellowClass = "";
+		if (this.state.clicked == "yellow") {
+			addYellowClass = "selected";
+		}
+
 		return (
 			<div>
 				<div className="sup text-center" />
 				<div className="container">
-					<div className="red-light" onClick={this.onchange} />
-					<div className="yellow-light" onClick={this.onchange} />
-					<div className="green-light" onClick={this.onchange} />
+					<div
+						className={"red-light " + addRedClass}
+						onClick={() => this.setState({ clicked: "red" })}
+					/>
+					<div
+						className={"yellow-light " + addYellowClass}
+						onClick={() => this.setState({ clicked: "yellow" })}
+					/>
+					<div
+						className={"green-light " + addGreenClass}
+						onClick={() => this.setState({ clicked: "green" })}
+					/>
 				</div>
 			</div>
 		);
